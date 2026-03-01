9acebd4137c1b34af1df9ca1aba8d667ec6e0d6e
sphenry2799 <sp.henry2799@gmail.com>
2026-03-01 18:04:04 +0000

    Add delivery threshold settings to settings.py

--- auraic/settings.py
+++ auraic/settings.py
@@ -154,6 +154,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+FREE_DELIVERY_THRESHOLD = 75
+STANDARD_DELIVERY_PERCENTAGE = 15
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
