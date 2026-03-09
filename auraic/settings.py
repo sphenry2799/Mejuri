483bef3118447615529d30eda01343ca3e728bca
sphenry2799 <sp.henry2799@gmail.com>
2026-03-09 18:36:51 +0000

    Add database for Heroku  to settings

--- auraic/settings.py
+++ auraic/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
+
 from pathlib import Path
 
 if os.path.isfile('env.py'):
@@ -31,9 +33,9 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
+    'auraic-2d04012354a2.herokuapp.com',
 
 ]
-
 
 # Application definition
 
@@ -128,15 +130,19 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'auraic.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
