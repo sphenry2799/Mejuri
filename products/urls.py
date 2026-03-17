884ded043f8ba12cd194dee92176870dd0945d5f
sphenry2799 <sp.henry2799@gmail.com>
2026-03-17 15:08:38 +0000

    Correct paths in products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'products'
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('add/', views.add_product, name='add_product'),    
-    path('<int:product_id>/', views.product_detail, name='product_detail'), 
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
